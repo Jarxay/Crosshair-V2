f2c5223886f17642dd6ec3290f3b4e10c3f40433
Exunys <76539058+Exunys@users.noreply.github.com>
2022-01-15 01:42:13 +0100

    Factory Reset Settings Manually

--- Resources/Scripts/Manual Factory Reset Settings.lua
+++ Resources/Scripts/Manual Factory Reset Settings.lua
@@ -1,25 +1,20 @@
-getgenv().Aimbot.Settings = {
+Environment.Settings = {
     SendNotifications = true,
     SaveSettings = true, -- Re-execute upon changing
     ReloadOnTeleport = true,
     Enabled = true,
-    TeamCheck = false,
-    AliveCheck = true,
-    WallCheck = false, -- Laggy
-    Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
-    TriggerKey = "MouseButton2",
-    Toggle = false,
-    LockPart = "Head" -- Body part to lock on
+    ToMouse = true
 }
 
-getgenv().Aimbot.FOVSettings = {
-    Enabled = true,
-    Visible = true,
-    Amount = 90,
-    Color = "255, 255, 255",
-    LockedColor = "255, 70, 70",
-    Transparency = 0.5,
-    Sides = 60,
+Environment.CrosshairSettings = {
+    Size = 12,
     Thickness = 1,
-    Filled = false
+    Color = "0, 255, 0",
+    Transparency = 1,
+    GapSize = 5,
+    CenterDot = false,
+    CenterDotColor = "0, 255, 0",
+    CenterDotSize = 1,
+    CenterDotTransparency = 1,
+    CenterDotFilled = true,
 }
